f52975655582f43ead3cf7b74b477ec816a1ee3e
Laura Garcia <lauraga41@gmail.com>
2016-05-28 12:39:49 -0500

    Add scaffold People

---
+++ test/factories/people.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :person do
+    first_name "MyString"
+    last_name "MyString"
+    email "MyString"
+    job "MyString"
+    bio "MyText"
+    gender 1
+    birthdate "2016-05-28"
+    picture "MyString"
+  end
+end
