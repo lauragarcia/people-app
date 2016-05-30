baf9769df4df15d13c35f9a09c6eabb4d6d214fa
Laura Garcia <lauraga41@gmail.com>
2016-05-30 10:43:30 -0500

    Ident

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,6 @@ module ApplicationHelper
 #
 # Return and string of a url image
 # If an object hasn't image, return default image url.
-
   def avatar_image(person)
     person.picture.present? ? person.picture.mid.url : 'default-avatar.jpg'
   end
